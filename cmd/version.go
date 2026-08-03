dda570068d1293675bed2aeac5affa1efc7f476b
Wather17 <215574773+Wather17@users.noreply.github.com>
2026-08-03 08:41:57 -0500

    feat(cmd): adicionar metadados de commit e date no comando version

--- cmd/version.go
+++ cmd/version.go
@@ -6,15 +6,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var Version = "0.1.0"
+var (
+	Version = "0.2.0"
+	Commit  = "none"
+	Date    = "unknown"
+)
 
-// NewVersionCmd cria o comando de versao.
+// NewVersionCmd cria o comando de versão.
 func NewVersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
-		Short: "Exibe a versao atual do p2t CLI",
+		Short: "Exibe a versão atual do p2t CLI",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Fprintf(cmd.OutOrStdout(), "p2t CLI v%s\n", Version)
+			out := cmd.OutOrStdout()
+			fmt.Fprintf(out, "p2t CLI v%s\n", Version)
+			if Commit != "none" {
+				fmt.Fprintf(out, " Commit: %s\n", Commit)
+			}
+			if Date != "unknown" {
+				fmt.Fprintf(out, " Build Date: %s\n", Date)
+			}
 		},
 	}
 }
